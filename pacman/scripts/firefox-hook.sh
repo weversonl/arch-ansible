9fb8d514b7fe0b6179d252fb7ba17e9791e91c0a
WeversonL <weversonlemos10@protonmail.com>
2023-05-08 15:02:03 -0300

    Update binaries, configs and setup steps

--- pacman/scripts/firefox-hook.sh
+++ pacman/scripts/firefox-hook.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sed -i '/Icon=/c\Icon=\/home\/verso\/.icons\/ico\/firefox.png' /usr/share/applications/firefox.desktop
+sed -i '/Exec=\/usr\/lib\/firefox\/firefox %u/c\Exec=env "GTK_IM_MODULE=xim" \/usr\/lib\/firefox\/firefox %u' /usr/share/applications/firefox.desktop
+sed -i '/--new-window/c\Exec=env "GTK_IM_MODULE=xim" firefox --new-window %u' /usr/share/applications/firefox.desktop
+sed -i '/--private-window/c\Exec=env "GTK_IM_MODULE=xim" firefox --private-window %u' /usr/share/applications/firefox.desktop
